de6ad302ef6bc16af6f9dada33d40a941a69f569
Igor Vicente <igor_machado05@hotmail.com>
2026-02-19 00:08:07 -0300

    files cached on client

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // Define o tempo mínimo de cache para imagens otimizadas (em segundos)
+    // 31536000 = 1 ano
+    minimumCacheTTL: 31536000,
+  },
+  async headers() {
+    return [
+      {
+        source: "/(favicon.ico|manifest.webmanifest)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=86400, stale-while-revalidate=3600",
+            // 86400 seg = 24h
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
